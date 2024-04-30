824647abb9bba83f12d7ee0013db2839c7e1d170
Abdul Amaan <amaan.programmer1@gmail.com>
2024-04-30 08:50:27 +0530

    added some extra font changes

--- dontSlouch/progressTab.py
+++ dontSlouch/progressTab.py
@@ -4,13 +4,13 @@ from tkinter import ttk
 class ProgressTab:
     def __init__(self, notebook, resetProgress):
         self.progressFrame = ttk.Frame(notebook)
-        self.progressLabel = ttk.Label(self.progressFrame, text = 'Progress', font= ('Arial', 40))
+        self.progressLabel = ttk.Label(self.progressFrame, text = 'Progress', font= ('Helvetica bold', 120))
     
-        self.title_label = ttk.Label(self.progressFrame, text = "Sit up straight! Get the high score!")
+        self.title_label = ttk.Label(self.progressFrame, text = "Sit up straight! Get the high score!",anchor="center",font= ('Helvetica bold', 120))
         self.title_label.pack()
-        self.score_label = ttk.Label(self.progressFrame)
+        self.score_label = ttk.Label(self.progressFrame,anchor="center",font= ('Helvetica bold', 120))
         self.score_label.pack()
-        self.max_score_label = ttk.Label(self.progressFrame)
+        self.max_score_label = ttk.Label(self.progressFrame,anchor="center",font= ('Helvetica bold', 120))
         self.max_score_label.pack()
         
         self.progressFrame.pack(padx=10, pady=10, anchor = 'w')
